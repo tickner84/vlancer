e2d636cc2871ecb3c2d6393f122141c7b23e9f40
Oliver Tickner <oliver.tickner@gmail.com>
2018-12-01 17:49:50 -0500

    Added tweets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Vlancer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    #use rails scaffolding generator
+	config.app_generators.scaffold_controller = :scaffold_controller
 
   end
 end
